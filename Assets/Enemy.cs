1c74786e93d1b108c9823e10caba2f5cf4403aa0
KevX36 <kevincard2007@gmail.com>
2026-09-14 11:23:47 -0300

    set path, made game manage to store path and other generic data

---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameManager gameManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
