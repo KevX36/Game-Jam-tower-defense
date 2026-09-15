73dad3c41870a13b3417a27b7166207d1f2308fc
KevX36 <kevincard2007@gmail.com>
2026-09-15 10:55:44 -0300

    added enemy movement

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,11 @@ public class Enemy : MonoBehaviour
 {
 
     public GameManager gameManager;
+    public Rigidbody rb;
+
+    public int speed = 3;
+
+    public Collider col;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,5 +23,19 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    IEnumerator Move()
+    {
+        for (int i = 1; i < gameManager.enemyPath.Count; i++)
+        {
+            Vector3 move = Vector3.MoveTowards(rb.position, gameManager.enemyPath[i].position, speed * Time.fixedDeltaTime);
+            Debug.Log("Moving");
+            rb.MovePosition(move);
+
+
+
+            yield return null;
+        }
     }
 }
