65c3f24262ff4e847f9caccb2803b1759955c39f
KevX36 <kevincard2007@gmail.com>
2026-09-22 13:57:19 -0300

    fixed bullets not despwning if enemies die before the bullet reaches it

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -8,29 +8,33 @@ public class Bullet : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
         this.gameObject.SetActive(false);
     }
-    
-
-    public Rigidbody2D rb;
-
-    public virtual void shoot(Enemy target, int power, float speed)
+    private void Update()
     {
-
-        Debug.Log("shot bullet");
-        StartCoroutine(Fly(target, power,speed));
+        if (Target == null)
+        {
+            Debug.Log("target died before it was reached");
+            this.gameObject.SetActive(false);
+        }
     }
 
-    IEnumerator Fly(Enemy Target, int power, float speed)
+    public Rigidbody2D rb;
+    public Enemy Target;
+    public virtual void shoot(Enemy target, int power, float speed)
+    {
+        Target = target;
+        Debug.Log("shot bullet");
+        StartCoroutine(Fly(power,speed));
+    }
+
+    IEnumerator Fly(int power, float speed)
     {
         while (Vector2.Distance(rb.position, Target.transform.position) > 0.9f && Target != null)
         {
+            
             Vector2 move = Vector2.MoveTowards(rb.position, Target.transform.position, speed * Time.fixedDeltaTime);
             Debug.Log("Moving towards" + Target.transform.position);
             rb.MovePosition(move);
-            if (Target == null)
-            {
-                Debug.Log("target died before it was reached");
-                this.gameObject.SetActive(false);
-            }
+            
 
 
             yield return null;
