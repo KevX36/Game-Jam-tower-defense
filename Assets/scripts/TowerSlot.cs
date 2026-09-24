20fee53c4dd3674c75a3540a72137d10312c38e8
KevX36 <kevincard2007@gmail.com>
2026-09-24 12:15:47 -0300

    saved rename

--- Assets/scripts/TowerSlot.cs
+++ Assets/scripts/TowerSlot.cs
@@ -46,7 +46,7 @@ public class TowerSlot : MonoBehaviour
         shopButtonText[0].text = $"archer ({towers[0].cost})";
         shopButtonText[1].text = $"Bolt ({towers[1].cost})";
         shopButtonText[2].text = $"Balasita ({towers[2].cost})";
-        shopButtonText[3].text = $"Bomb ({towers[3].cost})";
+        shopButtonText[3].text = $"Mage ({towers[3].cost})";
     }
     
 
